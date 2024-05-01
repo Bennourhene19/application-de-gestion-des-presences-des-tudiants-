c523453f532f6646fd3451ba4b81a07e20ce5de9
Bennourhene19 <127225311+Bennourhene19@users.noreply.github.com>
2024-05-01 10:14:47 +0100

    Update bdd.dart

--- bdd.dart
+++ bdd.dart
@@ -9,6 +9,75 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
+class Student {
+  final String firstName;
+  final String lastName;
+  final String section;
+  final String group;
+  final String subject;
+  bool isPresent;
+  List<int> grades;
+
+  Student(this.firstName, this.lastName, this.section, this.group, this.subject,
+      {this.isPresent = false, required this.grades});
+
+  // Méthode pour créer une instance de Student à partir d'un objet JSON
+  factory Student.fromJson(Map<String, dynamic> json) {
+    return Student(
+      json['firstName'],
+      json['lastName'],
+      json['section'],
+      json['group'],
+      json['subject'],
+      isPresent: json['isPresent'] ?? false,
+      grades: List<int>.from(json['grades'].map((x) => x as int)),
+    );
+  }
+
+  // Méthode pour convertir un objet Student en un objet JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'section': section,
+      'group': group,
+      'subject': subject,
+      'isPresent': isPresent,
+      'grades': grades,
+    };
+  }
+}
+
+List<Student> students = [
+  Student('John', 'Doe', 'Section A', 'Groupe 1', 'Mathématiques',
+      isPresent: true, grades: [12, 14, 11]),
+  Student('Jane', 'Smith', 'Section A', 'Groupe 1', 'Mathématiques',
+      isPresent: true, grades: [12, 14, 11]),
+  Student('Alice', 'Johnson', 'Section A', 'Groupe 1', 'Mathématiques',
+      isPresent: false, grades: [12, 14, 11]),
+  //------------
+  Student('Bob', 'Brown', 'Section A', 'Groupe 2', 'Physique',
+      isPresent: true, grades: [0, 0, 0]),
+  Student('Emily', 'Wilson', 'Section A', 'Groupe 2', 'Physique',
+      isPresent: true, grades: [0, 0, 0]),
+  Student('Michael', 'Taylor', 'Section A', 'Groupe 2', 'Physique',
+      isPresent: true, grades: [0, 0, 0]),
+  //------------
+  Student('David', 'Clark', 'Section A', 'Groupe 3', 'Chimie',
+      isPresent: true, grades: [0, 0, 0]),
+  Student('Sophia', 'Martinez', 'Section A', 'Groupe 3', 'Chimie',
+      grades: [0, 0, 0]),
+  Student('Olivia', 'Garcia', 'Section A', 'Groupe 3', 'Chimie',
+      isPresent: true, grades: [0, 0, 0]),
+  //------------
+  Student('Chloe', 'Brich', 'Section A', 'Groupe 4', 'Science',
+      grades: [0, 0, 0]),
+  Student('Brianna', 'Stew', 'Section A', 'Groupe 4', 'Science',
+      isPresent: true, grades: [0, 0, 0]),
+  Student('Nia', 'Lu', 'Section A', 'Groupe 4', 'Science',
+      isPresent: true, grades: [0, 0, 0]),
+];
+
 class User {
   final String id;
   final String name;
@@ -22,6 +91,7 @@ class User {
   final String section;
   final String group;
   final String schoolYear;
+  final String imageURL; // Champ pour l'URL de l'image
 
   User({
     required this.id,
@@ -36,6 +106,7 @@ class User {
     required this.section,
     required this.group,
     required this.schoolYear,
+    required this.imageURL, // Ajoutez cet argument au constructeur
   });
 
   static User? fromJson(Map<String, dynamic> user) {}
@@ -55,7 +126,9 @@ final List<User> validUsers = [
       specialty: 'isil',
       section: 'Section 1',
       group: 'Groupe 2',
-      schoolYear: '2024/2025'),
+      schoolYear: '2024/2025',
+      imageURL:
+          'URL_de_l_image'), // Remplacez 'URL_de_l_image' par l'URL réelle de l'image
 
   User(
       id: '2',
@@ -69,7 +142,8 @@ final List<User> validUsers = [
       specialty: 'isil',
       section: 'Section A',
       group: 'Groupe 2',
-      schoolYear: '2024/2025'),
+      schoolYear: '2024/2025',
+      imageURL: 'URL_de_l_image'),
 
   User(
       id: '3',
@@ -83,6 +157,55 @@ final List<User> validUsers = [
       specialty: 'isil',
       section: 'Section 1',
       group: 'Groupe 2',
-      schoolYear: '2024/2025'),
+      schoolYear: '2024/2025',
+      imageURL: 'URL_de_l_image'),
   // Ajoutez d'autres utilisateurs au besoin
 ];
+
+class Seance {
+  DateTime date;
+  String sujet;
+  List<Student> etudiants;
+  String salle;
+  String groupe; // Ajout du champ groupe
+  String matiere; // Ajout du champ matiere
+  String typeSeance; // Ajout du champ typeSeance
+
+  Seance({
+    required this.date,
+    required this.sujet,
+    required this.etudiants,
+    required this.salle,
+    required this.groupe,
+    required this.matiere,
+    required this.typeSeance,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'date': date.toIso8601String(),
+      'sujet': sujet,
+      'etudiants': etudiants.map((student) => student.toJson()).toList(),
+      'salle': salle,
+      'groupe': groupe,
+      'matiere': matiere,
+      'typeSeance': typeSeance,
+    };
+  }
+
+  static Seance fromJson(Map<String, dynamic> json) {
+    List<Student> etudiants = (json['etudiants'] as List)
+        .map((studentJson) => Student.fromJson(studentJson))
+        .toList();
+
+    return Seance(
+      date: DateTime.parse(json['date']),
+      sujet: json['sujet'],
+      etudiants: etudiants,
+      salle: json['salle'],
+      groupe: json['groupe'],
+      matiere: json['matiere'],
+      typeSeance: json['typeSeance'],
+    );
+  }
+}
